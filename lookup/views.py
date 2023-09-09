010d3e8164ab160f863a116ad57a62006b11a1f3
sunit chandra <sunitorchandra@gmail.com>
2023-09-09 23:47:29 +0530

    api styling

--- lookup/views.py
+++ lookup/views.py
@@ -12,7 +12,28 @@ def home(request):
     except Exception as e:
         api = "Error..."
 
-    return render(request, 'home.html', {'api' : api})
+    if api[0]["Category"]["Name"] == 'Good':
+        category_desctiption = "(0 - 50) Air quality is Good."
+        category_color = "good"
+    elif api[0]["Category"]["Name"] == 'Moderate':
+        category_desctiption = "(51 - 100) Air Quality is Moderate."
+        category_color = "moderate"
+    elif api[0]["Category"]["Name"] == 'Unhealthy for Sensitive Groups' :
+        category_desctiption = "(101 - 150) Air quality is Unhealthy for Sensitive Groups."
+        category_color = "usg"
+    elif api[0]["Category"]["Name"] == 'Unhealthy' :
+        category_desctiption = "(151 - 200) Air quality is Unhealthy."
+        category_color = "unhealthy"
+    elif api[0]["Category"]["Name"] == 'Very Unhealthy':
+        category_desctiption = "(201 - 300) Air quality is Very Unhealthy."
+        category_color = "veryunhealthy"
+    elif api[0]["Category"]["Name"] == 'Hazardous' :
+        category_desctiption = "(301+) Air quality is Hazardous."
+        category_color = "hazardous"
+
+    return render(request, 'home.html', {'api' : api, 
+                                         'category_desctiption' : category_desctiption,
+                                         'category_color' : category_color})
 
 def about(request):
     return render(request, 'about.html', {})
